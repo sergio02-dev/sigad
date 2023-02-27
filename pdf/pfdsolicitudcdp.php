8f67c79efe31f5058deb12d6aa2e617f8ece195a
sergio02-dev <serwar48@gmail.com>
2023-02-27 18:18:15 -0500

    pdf solicitud cdp

--- pdf/pfdsolicitudcdp.php
+++ pdf/pfdsolicitudcdp.php
@@ -56,68 +56,142 @@ function tildes($palabra){
     return $texto;
 }
 
+
+
 //////////////Yuliana 
 
 class MYPDF extends TCPDF {
     
 	public function Header(){
-        // Logo
-        $codigo="Código: FM-001";
-        $vigencia="Vigente desde: 01/04/2018";
-        $version="Versión: 2";
-        $style2 = array('width' => 0.3, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0));
-        $image_logousco = 'img/logohtml.png';
-        $this->setTextColor(0, 0, 0);
-        $this->SetDrawColor(0, 0, 0);
-        $this->SetLineWidth(0.1);
-        $this->setTextColor(0, 0, 0);
-        
-        $this->SetFillColor(255, 255, 127);
-        
-        $version='GABC-GA-04-01';
-
-        $nombrecolegio='GIMNASIO AMERICANO ABC';
-
-        $fchaa = date('Y-m-d');
-
-        $year_numm = substr($fchaa,0,4);
-        $mes_numm = substr($fchaa,5,2);
-        $dia_numm = substr($fchaa,8,2);
-
-        $fchh = "Fecha: ".$dia_numm."/".$mes_numm."/".$year_numm;
-        
-        $DTOS = 'DIAGNOSTICO';
-        $codigo = 'CODIGO';
-
-        // version
-        $this->SetFont('helvetica', ' ', 8);
-        $this->MultiCell(30.58, 33, ' ', 1, 'C', 0, 0, '', '', true, 0, false, true, 6, 'M');
-        
+       // Logo
+       $codigo="Código: FM-001";
+       $vigencia="Vigente desde: 01/04/2018";
+       $version="Versión: 2";
+       $style2 = array('width' => 0.3, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0));
+       $image_logousco = 'img/logohtml.png';
+       $this->setTextColor(0, 0, 0);
+       $this->SetDrawColor(0, 0, 0);
+       $this->SetLineWidth(0.1);
        
+       
+       $this->SetFillColor(255, 255, 127);
+       
+       $version='GABC-GA-04-01';
 
-        
+       $nombrecolegio='GIMNASIO AMERICANO ABC';
 
-        // modalidad y resolución
-        /*$this->SetFont('helvetica', ' ', 10);
-        $this->MultiCell(110, 20, 'EDUCACIÓN PREESCOLAR Y BÁSICA PRIMARIA'."\n".'Licencia de Funcionamiento por resolución 2725 de 2002' , 1, 'C', 0, 0, 50, 5, true, 1, false, true, 18, 'B');*/
-        
-        //imagen de la institución
-        $this->SetFont('helvetica', ' ', 8);
-        //$this->MultiCell(40, 30, ' ', 1, 'C', 0, 1, '', '', true);			
-        $this->Image($image_logousco, 7, 10, 25, 25, 'PNG', '', 'M', false, 120, '', false, false, 0, false, false, false);
+       $fchaa = date('Y-m-d');
 
-        // CODIGO
-        $this->SetFont('dejavusans', ' ', 8);
-        $this->MultiCell(55, 5, $codigo, 1, 'C', 0, 0, '', '', true);
+       $year_numm = substr($fchaa,0,4);
+       $mes_numm = substr($fchaa,5,2);
+       $dia_numm = substr($fchaa,8,2);
 
-        
-        /* informacion del formato
-        $this->MultiCell(40, 14, 'Revisión: V.6', 1, 'C', 0, 0, '', 11, true, 0, false, true, 14, 'M');
-        $this->SetFont('dejavusans', ' ', 12);
-        $this->MultiCell(110, 10, $DTOS, 1, 'C', 0, 1, 50, 25, true, 0, false, true, 10, 'M');
-        $this->SetFont('helvetica', ' ', 8);
-        $this->MultiCell(40, 10, $fchh, 1, 'C', 0, 0, '', 25, true, 0, false, true, 10, 'M');			
-			*/
+       $fchh = "Fecha: ".$dia_numm."/".$mes_numm."/".$year_numm;
+       
+       $DTOS = 'DIAGNOSTICO';
+       $codigo = 'CODIGO';
+       $titulo_universidad = 'UNIVERSIDAD SURCOLOMBIANA';
+       $titulo_cartera = 'GESTIÓN FINANCIERA Y DE RECURSOS FÍSICOS';
+       $titulo_formato='FORMATO ÚNICO SOLICITUD DE EXPEDICIÓN CERTIFICADO';
+       $subtitulo_formato = 'DE DISPONIBILIDAD PRESUPUESTAL - CDP';
+       $codigo_formato = 'AP-FIN-FO-08';
+       $version = 'VERSION';
+       $num_version = '8';
+       $vigencia = 'VIGENCIA';
+       $num_vigencia = '2023';
+       $pagina = 'PAGINA';
+       $num_pagina = '1 DE 1';
+
+       // version
+       $this->SetFont('helvetica', ' ', 8);
+       $this->MultiCell(30, 30, ' ', 1, 'C', 0, 0, '', '', true, 0, false, true, 6, 'M');
+
+       //imagen de la institución
+       $this->SetFont('helvetica', ' ', 8);
+       //$this->MultiCell(40, 30, ' ', 1, 'C', 0, 1, '', '', true);			
+       $this->Image($image_logousco, 7, 8, 25, 25, 'PNG', '', 'M', false, 120, '', false, false, 0, false, false, false);
+
+       // CODIGO
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(30, 7, $codigo, 1, 'C', 1, 2, '5', '35', true,0,false,true,5);
+
+       //TITULO UNIVERSIDAD
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(130, 7.5, $titulo_universidad, 1, 'C', 1, 2, '35', '5', true,0,false,true,5);
+
+       //TITULO CARTERA
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(130, 7.5, $titulo_cartera, 0, 'C', 1, 2, '35', '12', true,0,false,true,5);
+
+       //TITULO FORMATO
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(130, 7.5, $titulo_formato, 0, 'C', 1, 1, '35', '20', true,0,false,true,5);
+
+       //SUBTITULO FORMATO
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(130, 7.5, $subtitulo_formato, 0, 'C', 1, 1, '35', '27.2', true,0,false,true,5);
+
+       //CODIGO FORMATO
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 7);
+       $this->MultiCell(21.6, 7, $codigo_formato, 1, 'C', 1, 1, '35', '35', true,0,false,true,5);
+
+       
+       // VERSION
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(21.6, 7, $version, 1, 'C', 1, 2, '56.6', '35', true,0,false,true,5);
+
+       //NUMERO VERSION
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 7);
+       $this->MultiCell(21.6, 7, $num_version, 1, 'C', 1, 1, '78.2', '35', true,0,false,true,5);
+
+       // VIGENCIA
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(21.6, 7, $vigencia, 1, 'C', 1, 2, '99.8', '35', true,0,false,true,5);
+
+       //NUMERO VERSION
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 7);
+       $this->MultiCell(21.6, 7, $num_vigencia, 1, 'C', 1, 1, '121.4', '35', true,0,false,true,5);
+
+       //PAGINA
+       $this->SetFillColor(185, 33, 9);
+       $this->setTextColor(255, 255, 255);
+       $this->SetFont('dejavusans', 'B', 9);
+       $this->MultiCell(22, 7, $pagina, 1, 'C', 1, 2, '143', '35', true,0,false,true,5);
+
+       //NUMERO PAGINA
+       $this->SetFillColor(255, 255, 255);
+       $this->setTextColor(0,0,0);
+       $this->SetFont('dejavusans', 'B', 7);
+       $this->MultiCell(40, 7, $num_pagina, 1, 'C', 1, 1, '165', '35', true,0,false,true,5);
+
+       //IMAGEN ISO CALIDAD
+       $image_calidad = 'plantilla/img/log.png';
+       $this->setTextColor(0, 0, 0);
+       $this->SetDrawColor(0, 0, 0);
+       $this->SetLineWidth(0.1);
+
+       $this->MultiCell(40, 30, ' ', 1, 'C', 0, 0, '165', '5', true, 0, false, true, 6, 'M');
+       $this->Image($image_calidad, 170, 8, 30, 20, 'PNG', '', 'M', false, 120, '', false, false, 0, false, false, false);
     }
 
 	// Page footer
@@ -153,8 +227,6 @@ $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, 'Letter', true, 'UTF-8', false)
 ///////////////////////////////////
 
 $codigo_cdp = $_REQUEST['codigo_cdp'];
-
-include('crud/rs/rprte_slctud_cdp/rprte_slctud_cdp.php');
 
 
 
@@ -197,6 +269,11 @@ $pdf->SetFont('helvetica', '', 15);
 // add a page
 $pdf->AddPage();
 //echo '---->'.$codigo_acta;
+
+include('crud/rs/rprte_slctud_cdp/rprte_slctud_cdp.php');
+list($people,$car_nombre,$scdp_resolucion,$scdp_fecharesolucion,$scdp_numero,$scdp_consecutivo,$scdp_objeto,$valor_cdp) = $objRprteSlctudCdp->resolucionPersona($codigo_cdp);
+
+
 $html.='
     <table nobr="true" style="padding-left: 5px;" cellpadding="2">
     
@@ -205,19 +282,19 @@ $html.='
          
         </tr>
         <tr nobr="true">
-            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$nombreordenador_gasto.'<br>NOMBRE:</td>
+            <td style="width: 336px; height: 10px; font-size:70%; text-align:left; text-transform: uppercase;">'.$nombreordenador_gasto.'<br>NOMBRE:&nbsp;'.$people.'</td>
             
         </tr>
         <tr nobr="true">
-            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$cargoordenador_gasto.'<br>CARGO:</td>
+            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$cargoordenador_gasto.'<br>CARGO:&nbsp;'.$car_nombre.'</td>
             
         </tr>
         <tr nobr="true">
-            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$resolucionordenador_gasto.'<br>RESOLUCION ORDENACIÓN DEL GASTO No:</td>
+            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$resolucionordenador_gasto.'<br>RESOLUCION ORDENACIÓN DEL GASTO No:&nbsp;'.$scdp_resolucion.'</td>
       
         </tr>
         <tr nobr="true">
-            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$fechaordenador_gasto.'<br>FECHA:</td>
+            <td style="width: 336px; height: 10px; font-size:70%; text-align:left;">'.$fechaordenador_gasto.'<br>FECHA:&nbsp;'.$scdp_fecharesolucion.'</td>
        
         </tr>
 
@@ -225,12 +302,33 @@ $html.='
     <p style="font-size: 30px;"> &nbsp;</p>
 ';
 
+$ceros = '';
+
+  if($scdp_consecutivo <10){
+    $ceros = '0000';
+  }
+  else if ($scdp_consecutivo > 9 && $scdp_consecutivo <100){
+    $ceros = '000';
+  }
+  else if( $scdp_consecutivo >99 && $scdp_consecutivo <1000){
+    $ceros = '00';
+  }
+  else if( $scdp_consecutivo >999 && $scdp_consecutivo <10000){
+    $ceros = '0';
+    
+  }else{
+    $ceros = '';
+  }
+  $numero_solicitudCDP = $scdp_numero.'-'.$ceros.$scdp_consecutivo;
+
+
 $html.='
     <table nobr="true" style="padding-left: 5px;" cellpadding="2">
     
         <tr nobr="true">
            
-            <td style="width: 450px; height: 10px; font-size:70%; text-align:right; left:50;">'.$solicitud_numero.'<br>SOLICITUD No:</td>
+            <td style="width: 450px; height: 10px; font-size:70%; text-align:right; left:50;">'.$solicitud_numero.'<br>SOLICITUD No:&nbsp;'.$numero_solicitudCDP.'</td>
+
             <td style="width: 337px; height: 40px; font-size:70%; text-align:center;"><strong></strong></td>
         </tr>
         
@@ -251,16 +349,39 @@ $html.='
             <td style="width: 337px; height: 20px; font-size:70%; text-align:center;"><strong></strong></td>
         </tr>
         <tr nobr="true">
-            <td style="width: 100px; height: 10px; font-size:70%; text-align:left;">'.$objeto.'<br>OBJETO:</td>
-            <td style="border-left:1px solid black;border-bottom:1px solid black;border-top:1px solid black;border-right:1px solid black; height: 80px"></td>
+            <td style="width: 100px; height: 10px; font-size:70%; text-align:left;">'.$objeto.'<br>OBJETO:&nbsp;</td>
+            <td style="border-left:1px solid black;border-bottom:1px solid black;border-top:1px solid black;border-right:1px solid black; height: 80px; font-size:70%;">'.$scdp_objeto.'</td>
         </tr>
     </table>
     <p style="font-size: 30px;"> &nbsp;</p>
 ';  
 
 
+$excedenteDeFacultad = $objRprteSlctudCdp->excedenteFacultad($codigo_cdp);
 
+if($excedenteDeFacultad==1){
 $html.='
+    <table nobr="true" style="padding-left: 5px;" cellpadding="2">
+        
+        <tr nobr="true">
+            <td style="width: 337px; height: 20px; font-size:70%; text-align:center;"><strong></strong></td>
+        </tr>
+        <tr nobr="true">
+            <td style="width: 200px; height: 40px; font-size:70%; text-align:left;">'.$excedentes_facultad.'<br>EXCEDENTES DE FACULTAD:</td>
+            <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center; padding: 2px">SI</td>
+            <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center">X</td>
+            <td style="border-collapse: collapse;margin:0px;border:1px solid black;  height: 2px; width: 30px; font-size:70%; text-align:center">NO</td>
+            <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center"></td>
+        
+        </tr>
+
+
+    </table>
+    <p style="font-size: 30px;"> &nbsp;</p>
+';
+}
+else{
+    $html.='
     <table nobr="true" style="padding-left: 5px;" cellpadding="2">
         
         <tr nobr="true">
@@ -271,7 +392,7 @@ $html.='
             <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center; padding: 2px">SI</td>
             <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center"></td>
             <td style="border-collapse: collapse;margin:0px;border:1px solid black;  height: 2px; width: 30px; font-size:70%; text-align:center">NO</td>
-            <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center"></td>
+            <td style="border-collapse: collapse;margin:0px;border:1px solid black; height: 2px; width: 30px; font-size:70%; text-align:center">X</td>
         
         </tr>
 
@@ -279,23 +400,57 @@ $html.='
     </table>
     <p style="font-size: 30px;"> &nbsp;</p>
 ';
+}
 
+$lista_poai = $objRprteSlctudCdp->poai($codigo_cdp);
+  $num_registro=25;
+  $id_registro=1;
+  if($lista_poai){
+    foreach ($lista_poai as $data_lista_etapa) {
+      $poa_referencia = $data_lista_etapa['poa_referencia'];
+      $poa_numero = $data_lista_etapa['poa_numero'];
+      $esc_valor = $data_lista_etapa['esc_valor'];
+      $esc_clasificador = $data_lista_etapa['esc_clasificador'];
+      $esc_dane = $data_lista_etapa['esc_dane'];
+      
+      
+      $str = $esc_clasificador;
+      $numero_caracteres = strlen($str); 
+      $desde = $numero_caracteres - 2;
+      $ultimos_caracteres= substr($str,$desde,2);
+
+      $fuente = $objRprteSlctudCdp->fuentes_financiacionCDP($ultimos_caracteres);
+      
+      $poa_etapa = $poa_referencia." ".$poa_numero;
 
 $html.='
     <table nobr="true" style="padding-left: 5px; " cellpadding="2">
 
-    <tr nobr="true">
+        <tr nobr="true">
             <td style="width: 337px; height: 40px; font-size:70%; text-align:center;"><strong></strong></td>
         </tr>
     
         <tr nobr="true">
            
-            <th style="width: 140px; height: 10px; font-size:70%; text-align:left;">'.$plan_accion.'<br>PLAN DE ACCIÓN:</th>
+            <th style="width: 160px; height: 10px; font-size:70%; text-align:left;">'.$plan_accion.'<br>PLAN DE ACCIÓN:</th>
             <th style="width: 80px;  font-size:60%; text-align:center;padding-top: 5px ">'.$valor.'<br>VALOR</th>
             <th style="width: 150px; height: 10px; font-size:60%; text-align:center;padding-top: 5px ">'.$codigo_presupuestal.'<br>CODIGO PRESUPUESTAL</th>
             <th style="width: 80px;  font-size:60%; text-align:center;padding-top: 5px ">'.$codigo_dane.'<br>CODIGO DANE</th>
             <th style="width: 150px; height: 10px; font-size:60%; text-align:center;padding-top: 5px ">'.$fuente_financiacion.'<br>FUENTE DE FINANCIACIÓN</th>
             <th style="width: 80px;  font-size:60%; text-align:center;padding-top: 5px ">'.$etapa_actividad.'<br>ESTAPA DE LA ACTIVIDAD No.</th>
+           
+        </tr>
+
+       
+
+        <tr nobr="true">
+           
+            <th style="width: 160px; height: 10px; font-size:60%; text-align:center;padding-top: 5px ">'.$valor.'<br></th>
+            <th style="width: 80px; height: 10px; font-size:60%; text-align:center;padding-top: 5px ">'.$valor.'<br>'.$esc_valor.'</th>
+            <th style="width: 150px; font-size:60%; text-align:center;padding-top: 5px ">'.$codigo_presupuestal.'<br>'.$esc_clasificador.'</th>
+            <th style="width: 80px;  font-size:60%; text-align:center;padding-top: 5px ">'.$codigo_dane.'<br>'. $esc_dane.'</th>
+            <th style="width: 150px; height: 10px; font-size:60%; text-align:center;padding-top: 5px ">'.$fuente_financiacion.'<br>'.$fuente.'</th>
+            <th style="width: 80px;  font-size:60%; text-align:center;padding-top: 5px ">'.$etapa_actividad.'<br>'.$poa_etapa.'</th>
            
         </tr>
         
@@ -307,6 +462,8 @@ $html.='
     </table>
     <p style="font-size: 30px;"> &nbsp;</p>
 ';
+  }
+}
 
 $html.='
     <table nobr="true" style="padding-left: 5px;" >
@@ -319,6 +476,7 @@ $html.='
         </tr>
         <tr nobr="true">
             <td style="width: 200px; height: 40px; font-size:70%; text-align:left;">'.$valor_total.'<br><strong>VALOR TOTAL SOLICITADO:</strong></td>
+            <td style="width: 200px; height: 40px; font-size:60%; text-align:left;">'.$valor_total.'<br>'.$valor_cdp.'</td>
         </tr>
         <tr nobr="true">
             <td style="width: 300px; height: 20px; font-size:60%; text-align:center;">'.$vigencia_cdp.'<br><strong>VIGENCIA DEL CDP:</strong></td>
