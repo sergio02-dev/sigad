c6baf3cb46b5dc6c4d34422a898ab89c1b7d93b1
lightwood <sebastiandias1999@gmail.com>
2023-03-04 16:49:02 -0500

    archivos para consultar cdp y rp

---
+++ grlla/cnsltarrp/cnsltarrp.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include('prncpal/hd.php'); ?>
+    <link rel="stylesheet" href="DataTables/datatables.min.css" />
+    <script src="DataTables/datatables.min.js"></script>
+
+	<style>
+		div#selectvigencia {
+			position: absolute;
+			margin-top: 4px;
+			margin-left: 68%;
+			height: 24px;
+			z-index: 1000;
+		}
+        .modal-body {
+            max-height: calc(100vh - 210px);
+            overflow-y: auto;
+        }
+	</style>
+</head>
+
+<body>
+	<div class="page-container" style='padding:0; margin:0;'>
+
+		<div  class="container-fluid">
+			<div class="row">
+				<div class="col-sm-3">
+
+				<!-- INICIO MENU -->
+					<?php include('prncpal/mnu.php') ?>
+				<!--..........................................FIN MENU..................................................-->
+
+				</div>
+
+				<div class="col-sm-9 container-principal" >
+					<div class="col-sm-12 modal-header capa_titulo"><h2><strong>CONSULTAR RP</strong> </h2></div>
+
+					<div class="col-sm-12">&nbsp;</div>
+					<?php $visibilidad=$_SESSION['visibilidadBotones']; ?>
+
+                    <div class="col-sm-12" id="dataCdp">
+                        <?php 
+                            include('grlla/data/cdp/cdp.php');
+                        ?>
+                    </div>
+						
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
