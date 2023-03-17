3edc8830395fd4c9b387999c249e001bbf83e49a
sergio02-dev <serwar48@gmail.com>
2023-03-17 17:35:52 -0500

    pdf corregido

--- pdf/pfdsolicitudcdp.php
+++ pdf/pfdsolicitudcdp.php
@@ -1,9 +1,5 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
-use Mpdf\Tag\Center;
 
 set_time_limit(1800000000);
 require_once('lbr/tcpdf_hefo/config/lang/eng.php');
